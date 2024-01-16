4981438d9aea9146ef9f15a7ac18cc95801e58e5
AlexeySitnikov <alexey.sitnikov@bk.ru>
2024-01-16 16:30:48 +0300

    trying to read files by promise

--- src/CustomHooks/useRows.js
+++ src/CustomHooks/useRows.js
@@ -7,42 +7,83 @@ const { TxtReader } = require('txt-reader')
 export const useRows = ({ selectedFiles }) => {
   // const [Ex, setEx] = useState()
 
-  useEffect(() => {
-    const f = new Promise((resolve) => {
-      const files = [...Array.from(selectedFiles)]
-      const fullX = []
-      const fullY = []
-      const fullZ = []
-      files.forEach((row, index) => {
-        const reader = new TxtReader()
-        let linesNumber = 0
-        reader.loadFile(row).then((res) => {
-          linesNumber = res.result
-        })
-          .then(() => {
-            reader.getLines(3, linesNumber)
-              .then((response) => {
-                fullX[index] = []
-                fullY[index] = []
-                fullZ[index] = []
+  const getFiles = () => [...Array.from(selectedFiles)]
 
-                for (let i = 0; i < response.result.length; i += 1) {
-                  fullX[index].push(`${response.result[i].trim().replace(/\s\s+/g, ' ').split(' ')[2]}\n`)
-                  fullY[index].push(`${response.result[i].trim().replace(/\s\s+/g, ' ').split(' ')[5]}\n`)
-                  fullZ[index].push(`${response.result[i].trim().replace(/\s\s+/g, ' ').split(' ')[7]}\n`)
-                }
-              })
-          })
+  useEffect(() => {
+    const getLinesNumber = async (row) => (
+      new Promise((resolve) => {
+        const reader = new TxtReader()
+        reader.loadFile(row).then((res) => {
+          resolve(res.result)
+        })
       })
-      resolve(fullX)
-    })
+    )
+
+    const getLines = (row, linesNumber) => (
+      new Promise((resolve) => {
+        const reader = new TxtReader()
+        reader.loadFile(row).then(() => {
+          reader.getLines(3, linesNumber).then((res) => {
+            resolve(res)
+          })
+        })
+      })
+    )
+
+    const fillArray = (row) => (
+      new Promise((resolve) => {
+        const as = []
+        for (let i = 0; i < row.length; i += 1) {
+          as.push(`${row[i].trim().replace(/\s\s+/g, ' ').split(' ')[2]}\n`)
+        }
+        resolve(as)
+      })
+    )
+
+    // const f = new Promise((resolve) => {
+    //   const files = [...Array.from(selectedFiles)]
+    //   const fullX = []
+    //   const fullY = []
+    //   const fullZ = []
+    //   files.forEach((row, index) => {
+    //     const reader = new TxtReader()
+    //     let linesNumber = 0
+    //     reader.loadFile(row).then((res) => {
+    //       linesNumber = res.result
+    //     })
+    //       .then(() => {
+    //         reader.getLines(3, linesNumber)
+    //           .then((response) => {
+    //             resolve(response)
+    //             fullX[index] = []
+    //             fullY[index] = []
+    //             fullZ[index] = []
+
+    //             for (let i = 0; i < response.result.length; i += 1) {
+    //               fullX[index].push(`${response.result[i].trim().replace(/\s\s+/g, ' ')
+    // .split(' ')[2]}\n`)
+    //               fullY[index].push(`${response.result[i].trim().replace(/\s\s+/g, ' ')
+    // .split(' ')[5]}\n`)
+    //               fullZ[index].push(`${response.result[i].trim().replace(/\s\s+/g, ' ')
+    // .split(' ')[7]}\n`)
+    //             }
+    //           })
+    //       })
+    //   })
+    // })
 
     if (selectedFiles) {
-      const a = () => {
-        f.then((result) => console.log(result[0]))
-      }
-      a()
-      // setEx([1])
+      const files = getFiles()
+      Promise.all([...files.map((row) => getLinesNumber(row))])
+        .then((res) => {
+          res.forEach((file, index) => {
+            getLines(files[index], file.lineCount)
+              .then((response) => {
+                fillArray(response.result)
+                  .then((r) => (console.log(r)))
+              })
+          })
+        })
     }
   }, [selectedFiles])
 
